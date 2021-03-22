0f063aa228a7d1f87870f2521e43052c335fb19f
Francisco <panchoseur@gmail.com>
2021-03-22 11:30:47 -0300

    first commit

---
+++ migr_robot_millas/src/main/java/com/latam/millas/MigrRobotMillasApplication.java
@@ -0,0 +1,13 @@
+package com.latam.millas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrRobotMillasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigrRobotMillasApplication.class, args);
+	}
+
+}
---
+++ migr_robot_millas/src/test/java/com/latam/millas/MigrRobotMillasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.latam.millas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrRobotMillasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
