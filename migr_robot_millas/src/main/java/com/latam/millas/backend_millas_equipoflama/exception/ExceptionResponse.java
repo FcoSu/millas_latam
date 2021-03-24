02fea99a7e05fb4468bd9409ee3a11a2c289d490
Francisco <panchoseur@gmail.com>
2021-03-24 12:59:50 -0300

    validaciones varias

---
+++ migr_robot_millas/src/main/java/com/latam/millas/backend_millas_equipoflama/exception/ExceptionResponse.java
@@ -0,0 +1,11 @@
+package com.latam.millas.backend_millas_equipoflama.exception;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class ExceptionResponse {
+	 private String mensaje;
+     private LocalDateTime dateTime;
+}
--- migr_robot_millas/src/main/java/com/latam/millas/backforfront_millas_equipoflama/controller/MillasController.java
+++ migr_robot_millas/src/main/java/com/latam/millas/backforfront_millas_equipoflama/controller/MillasController.java
@@ -1,5 +1,6 @@
 package com.latam.millas.backforfront_millas_equipoflama.controller;
 
+import java.time.LocalDateTime;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,12 +12,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.latam.millas.backend_millas_equipoflama.exception.ExceptionResponse;
 import com.latam.millas.backend_millas_equipoflama.service.MillasService;
 import com.latam.millas.backend_millas_equipoflama.service.UsuarioService;
 import com.latam.millas.dal_millas_equipoflama.dto.MillasDto;
 import com.latam.millas.dal_millas_equipoflama.dto.UsuarioDto;
 import com.latam.millas.dal_millas_equipoflama.dto.json;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/api/millas_latam")
 public class MillasController {
@@ -28,30 +33,65 @@ public class MillasController {
 
 	@Autowired
 	private MillasService Millas;
+	ExceptionResponse response = new ExceptionResponse();
 
 	@RequestMapping(value = "/Millas/Post", method = RequestMethod.POST, consumes = "application/json")
-	public ResponseEntity<MillasDto> getData(@RequestBody json Json) {
+	public ResponseEntity<Object> getData(@RequestBody json Json) {
 		pnr = Json.getPnr();
 		UsuarioEmail = Json.getUserEmail();
 		UsuarioDto Usuario = null;
 		MillasDto MillasDTO = null;
 
-		if (ValidarFormatoEmail(Json.getUserEmail())) {
-			Usuario = usuarioService.ValidarUsuario(Json.getUserEmail());
-			if (Usuario.getCodigoUsuario() != 0) {
-			
-				
-				MillasDTO = Millas.ListarMillas(pnr,UsuarioEmail);
-				System.out.println(MillasDTO);
+		if (Json.getPnr().equals("")) {
+			log.error("PNR no debe estar vacio");
+			response.setDateTime(LocalDateTime.now());
+			response.setMensaje("PNR no debe estar vacio");
+			ResponseEntity<Object> entity = new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+			return entity;
+		}
 
-				return new ResponseEntity<>(MillasDTO, HttpStatus.OK);
-			} else {
-				return new ResponseEntity<>(MillasDTO, HttpStatus.BAD_REQUEST);
-			}
-		} else
+		if (Json.getUserEmail().equals("")) {
+			log.error("Correo no debe estar vacio");
+			response.setDateTime(LocalDateTime.now());
+			response.setMensaje("Correo no debe estar vacio");
+			ResponseEntity<Object> entity = new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+			return entity;
+		}
 
+		if (ValidarFormatoEmail(Json.getUserEmail().toString()) == false) {
+			log.error("Correo no es valido");
+			response.setDateTime(LocalDateTime.now());
+			response.setMensaje("Correo no es valido");
+			ResponseEntity<Object> entity = new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+			return entity;
+		}
+		
+
+		if (Json.getPnr().length() != 6) {
+			log.error("PNR Solo debe tener 6 caracteres");
+			response.setDateTime(LocalDateTime.now());
+			response.setMensaje("PNR Solo debe tener 6 caracteres");
+			ResponseEntity<Object> entity = new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+			return entity;
+		}
+
+		Usuario = usuarioService.ValidarUsuario(Json.getUserEmail());
+		if (Usuario.getCodigoUsuario() == 0) {
+			log.error("Correo no existe en los datos de latam");
+			response.setDateTime(LocalDateTime.now());
+			response.setMensaje("Correo no existe en los datos de latam");
+			ResponseEntity<Object> entity = new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+			return entity;
+		}
+		if (Usuario.getCodigoUsuario() != 0) {
+
+			MillasDTO = Millas.ListarMillas(pnr, UsuarioEmail);
+			System.out.println(MillasDTO);
+
+			return new ResponseEntity<>(MillasDTO, HttpStatus.OK);
+		} else {
 			return new ResponseEntity<>(MillasDTO, HttpStatus.BAD_REQUEST);
-
+		}
 	}
 
 	public boolean ValidarFormatoEmail(String Email) {
